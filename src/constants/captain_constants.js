42cc663d74b25b27e1b77f51694e6cade35fdf0a
Joseph P DeBartola III <joe@codeofjoe.com>
2015-12-08 05:25:03 -0600

    Added constants for Captain action types.

---
+++ src/constants/captain_constants.js
@@ -0,0 +1,8 @@
+import { createConstants } from '../utils';
+
+export default createConstants(
+    'TOGGLE_NAV_DROPDOWN',
+    'TOGGLE_LIVE_MODE',
+    'INCREMENT_PERSON_COUNTER',
+    'DECREMENT_PERSON_COUNTER'
+);
