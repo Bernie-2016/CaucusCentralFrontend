ab5fad395b4db77f8151f767b402980cedbe89ad
Joseph P DeBartola III <joe@codeofjoe.com>
2015-12-09 17:48:24 -0600

    Updated and stylized live person counter a bit.

--- src/components/captain/dashboard/CaptainDashboardLiveCounter.js
+++ src/components/captain/dashboard/CaptainDashboardLiveCounter.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import './CaptainDashboardLiveCounter.scss';
 
-class CaptainDashboardLiveCounter extends React.component {
-
-    render() {
+var CaptainDashboardLiveCounter = React.createClass({
+    
+    render: function() {
         return (
-            <span className="glyphicon glyphicon-chevron-up counter-button" aria-hidden="true"
-                onClick={this.props.actions.increment}></span>
-            <span className="counter-text">
-            { this.props.counter_value }
-            </span>
-            <span className="glyphicon glyphicon-chevron-down counter-button" aria-hidden="true"
-                onClick={this.props.actions.decrement}></span>
+            <div className="col-xs-5 text-center">
+              <span className="glyphicon glyphicon-chevron-up
+                               counter-button"
+                    aria-hidden="true"
+                    onClick={this.props.actions.increment}>
+              </span>
+              <br />
+              <div className="counter-text">{ this.props.counterValue }</div>
+              <br />
+              <span className="glyphicon glyphicon-chevron-down
+                               counter-button"
+                    aria-hidden="true"
+                    onClick={this.props.actions.decrement}>
+              </span>
+            </div>
         );
     }
 
-}
+});
 
 export default CaptainDashboardLiveCounter;
