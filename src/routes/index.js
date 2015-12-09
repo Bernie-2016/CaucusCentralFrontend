ec3698631a23adfcac3371cb5bd05a3f9504d9a9
Joseph P DeBartola III <joe@codeofjoe.com>
2015-12-09 17:44:49 -0600

    Updated routes so dormant dashboard and live mode are on same level. Imported and inserted CaptainLayout and Live View for testing.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,18 +4,20 @@ import CoreLayout            from 'layouts/CoreLayout';
 import HomeView              from 'views/HomeView';
 import AboutView             from 'views/AboutView';
 
+import CaptainLayout         from 'layouts/captain/CaptainLayout';
+import CaptainDashboardLiveView from 'views/captain/dashboard/CaptainDashboardLiveView';
+
 export default (
   <Route        component={CoreLayout} path='/'>
     <IndexRoute component={HomeView} />
     <Route      component={AboutView}  path='/about' />
-    <Route      component={undefined} path='captain'>
+    <Route      component={CaptainLayout} path='captain'>
         <Route      component={undefined} path='/login' />
         <Route      component={undefined} path='/verify/:key' />
         <Route      component={undefined} path='/prepare' />
         <Route      component={undefined} path='/how-to-use' />
-        <Route      component={undefined} path='/dashboard'>
-            <Route      component={undefined} path='/live'/>
-        </Route>
+        <Route      component={undefined} path='/dashboard' />
+        <Route      component={CaptainDashboardLiveView} path='live'/>
     </Route>
   </Route>
 );
