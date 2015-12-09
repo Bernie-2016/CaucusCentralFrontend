c28ec14250b0854b6b5e2d6ff49fdb101e15e07f
Joseph P DeBartola III <joe@codeofjoe.com>
2015-12-08 20:13:50 -0600

    Updated constants to reflect simplification of react flow for Captain side.

--- src/constants/captain_constants.js
+++ src/constants/captain_constants.js
@@ -1,8 +1,6 @@
 import { createConstants } from '../utils';
 
 export default createConstants(
-    'TOGGLE_NAV_DROPDOWN',
-    'TOGGLE_LIVE_MODE',
     'INCREMENT_PERSON_COUNTER',
     'DECREMENT_PERSON_COUNTER'
 );
