3633fe68c69704af4163dc376440c164b3dba7ca
Joseph P DeBartola III <joe@codeofjoe.com>
2015-12-16 13:26:14 -0600

    Added new view for dashboard to coincide with mockups.

---
+++ src/views/captain/dashboard/CaptainDashboardView.jsx
@@ -0,0 +1,43 @@
+import React                  from 'react';
+import { bindActionCreators } from 'redux';
+import { connect }            from 'react-redux';
+import captainActions         from 'actions/captain_actions';
+import CaptainAttendeeInputs  from 'components/captain/CaptainAttendeeInputs';
+
+const mapStateToProps = (state) => ({
+  precinct_name: 'Altoona 4',
+  total_delegates: 4
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(captainActions, dispatch)
+});
+
+var CaptainDashboardView = React.createClass({
+
+    render: function() {
+        return (
+          <div className="container-fluid">
+
+            <div className="row">
+              <div className="col-xs-6 text-left">
+                { this.props.precinct_name }
+              </div>
+              <div className="col-xs-6 text-right">
+                { this.props.total_delegates }
+              </div>
+            </div>
+
+            {/* Insert Viability Component Here */}
+
+            <div className="row">
+              <CaptainAttendeeInputs />
+            </div>
+
+          </div>
+        );
+    }
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CaptainDashboardView);
