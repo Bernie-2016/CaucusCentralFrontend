4629bebe5384350a410033bae9854d7b1803205f
Ryan Carey <r.carey@wks-170.corp.pao.dailymotion.com>
2016-01-06 20:28:17 -0800

    add es6-promise back and update captain_actions

--- src/actions/captain_actions.js
+++ src/actions/captain_actions.js
@@ -14,28 +14,30 @@ export default {
     }
   }),
   tallyAttendees: (payload) => {
-    let body = {
+    const body = {
       precinct: {
         id: payload.precinctId,
         total_attendees: payload.attendees,
-        delegate_counts: []
+        delegate_counts: _.reduce(payload.candidates, (result, supporters, candidate) => {
+          return result.concat([{
+            key: candidate,
+            supporters: supporters
+          }]);}, [])
       },
       authentication: `BASIC ${window.__AUTH_TOKEN__}`,
       attendees: payload.attendees
     };
-    _.forEach(payload.candidates, (supporters, candidate) => {
-      body.precinct.delegate_counts.push({
-        key: candidate,
-        supporters: supporters
-      });
-    });
+    console.log('BODY', body);
     return {
       [CALL_API]: {
         types: [{ type: c.TALLY_ATTENDEES_REQUEST, payload: payload },
                 c.TALLY_ATTENDEES_SUCCESS,
                 c.TALLY_ATTENDEES_FAILURE],
-        endpoint: formatEndpoint(`/precincts/${payload.precinctId}`),
-        method: 'PATCH',
+        endpoint: formatEndpoint(`/precincts/${payload.precinctId}/viability`),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
         body: body
       }
     };
