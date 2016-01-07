76a3675ebb96f806c59a9d2dcf5f38056899c32a
Ryan Carey <r.carey@wks-170.corp.pao.dailymotion.com>
2016-01-06 21:06:46 -0800

    add session reducer

--- src/reducers/session/index.js
+++ src/reducers/session/index.js
@@ -1,33 +1,48 @@
 import { createReducer } from '../../utils';
+import * as c from 'constants/session';
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: undefined,
+  privilege: undefined,
+  token: undefined,
+  fetching: false,
+  destroying: false
+};
 
 const sign = {
   in: {
-    request: function () {
-
+    request: function (state, payload) {
+      return Object.assign({}, state, {
+        fetching: true,
+        email: payload.email
+      });
     },
-    success: function () {
-
+    success: function (state, response) {
+      return Object.assign({}, state, {
+        fetching: false,
+        firstName: response.first_name,
+        lastName: response.last_name,
+        privilege: response.privilege,
+        token: response.token
+      });
     },
-    failure: function () {
-
+    failure: function (state, error) {
+      return Object.assign({}, state, { error: error, fetching: false });
     }
   },
   out: {
-    request: function () {
-
+    request: function (state) {
+      return Object.assign({}, state, { destroying: true });
     },
-    success: function () {
-
+    success: function (state) {
+      return Object.assign({}, state, initialState);
     },
-    failure: function () {
-
+    failure: function (state, error) {
+      return Object.assign({}, state, { error: error, destroying: false });
     }
   }
-};
-
-const initialState = {
-  authToken: undefined,
-  isAdmin: false
 };
 
 export default createReducer(initialState, {
