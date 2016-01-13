a20e9633e57576ee9ec383341a630fec58f66c62
Zach Schneider <zach@schneid.io>
2016-01-13 15:31:10 -0600

    Style signin form

--- src/components/sign-in/SignInForm.js
+++ src/components/sign-in/SignInForm.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import './SignInForm.scss';
 
 export class SignInForm extends React.Component {
   render () {
     return (
-      <form onSubmit={this.props.onSubmit}>
-        <div className='form-group'>
-          <label htmlFor='email'>Email</label>
-          <input type='email' className='form-control' id='email' value={this.props.email} onChange={this.props.onEmailChange} />
-        </div>
-        <div className='form-group'>
-          <label htmlFor='password'>Password</label>
-          <input type='password' className='form-control' id='password' onChange={this.props.onPasswordChange} />
-        </div>
-        <button type='submit' className='btn btn-default'>Sign In</button>
-      </form>
+      <div className="container signin">
+        <form className="form-signin" onSubmit={this.props.onSubmit}>
+          <h2 className="form-signin-heading">Sign in</h2>
+          <label htmlFor="email" className="sr-only">Email</label>
+          <input type="email" id="email" className="form-control" placeholder="Email" value={this.props.email} onChange={this.props.onEmailChange} required autofocus />
+          <label htmlFor="password" className="sr-only">Password</label>
+          <input type="password" id="password" className="form-control" placeholder="Password" onChange={this.props.onPasswordChange} required />
+          <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+      </div>
     );
   }
 }
