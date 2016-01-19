c1e9539ba88582847f407efe35695b92b2967bd1
Zach Schneider <zach@schneid.io>
2016-01-19 12:35:32 -0600

    Fix sign in binding

--- src/components/sign-in/SignInForm.js
+++ src/components/sign-in/SignInForm.js
@@ -23,9 +23,9 @@ export class SignInForm extends React.Component {
         <form className="form-signin" onSubmit={ (e) => this.onSubmit(e) }>
           <h2 className="form-signin-heading">Sign in</h2>
           <label htmlFor="email" className="sr-only">Email</label>
-          <input type="email" id="email" className="form-control" placeholder="Email" value={this.props.email} onChange={ (e) => this.onUpdate(e) } required autofocus />
+          <input type="email" name="email" className="form-control" placeholder="Email" value={this.props.email} onChange={ (e) => this.onUpdate(e) } required autofocus />
           <label htmlFor="password" className="sr-only">Password</label>
-          <input type="password" id="password" className="form-control" placeholder="Password" value={this.props.password} onChange={ (e) => this.onUpdate(e) } required />
+          <input type="password" name="password" className="form-control" placeholder="Password" value={this.props.password} onChange={ (e) => this.onUpdate(e) } required />
           <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         </form>
       </div>
