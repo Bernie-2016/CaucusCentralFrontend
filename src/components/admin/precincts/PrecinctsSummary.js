f8c983274abe05fce68430e29fcbac16eef5887b
Zach Schneider <zach@schneid.io>
2016-01-19 19:43:58 -0600

    Add CSV download

--- src/components/admin/precincts/PrecinctsSummary.js
+++ src/components/admin/precincts/PrecinctsSummary.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React              from 'react';
+import { Link }           from 'react-router';
+import { formatEndpoint } from 'utils/api';
 
 export class PrecinctsSummary extends React.Component {
   getTotalCountsFor (candidate) {
@@ -27,26 +29,39 @@ export class PrecinctsSummary extends React.Component {
   }
 
   render() {
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Total Delegates</th>
-            <th>Bernie Sanders</th>
-            <th>Hillary Clinton</th>
-            <th>Martin O'Malley</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{this.getTotalDelegates()}</td>
-            <td>{this.getTotalCountsFor('sanders')}</td>
-            <td>{this.getTotalCountsFor('clinton')}</td>
-            <td>{this.getTotalCountsFor('omalley')}</td>
-          </tr>
-        </tbody>
-      </table>
-    );
+    if(this.props.fetched) {
+      return (
+        <div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Total Delegates</th>
+                <th>Bernie Sanders</th>
+                <th>Hillary Clinton</th>
+                <th>Martin O'Malley</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{this.getTotalDelegates()}</td>
+                <td>{this.getTotalCountsFor('sanders')}</td>
+                <td>{this.getTotalCountsFor('clinton')}</td>
+                <td>{this.getTotalCountsFor('omalley')}</td>
+              </tr>
+            </tbody>
+          </table>
+          <p>
+            <a href={formatEndpoint('/states/' + this.props.state.code + '/csv?token=' + this.props.sessionToken)} target='_blank'>
+              Download Results CSV
+            </a>
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div />
+      );
+    }
   }
 }
 
