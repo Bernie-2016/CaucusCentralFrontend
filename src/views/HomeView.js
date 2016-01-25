1793a21a2a5c6efd81a187b77d7acb812ec84983
Zach Schneider <zach@schneid.io>
2016-01-24 18:28:34 -0600

    Update homepage copy

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -10,7 +10,7 @@ export default class HomeView extends React.Component {
           Welcome to Caucus Central, the official campaign app to collect live data from caucus precincts. We designate precinct captains to officially report on totals from their precinct, and we also accept crowdsourced data from all Bernie supporters to get the most up-to-the-minute results.
         </p>
         <p>
-          If you're a precinct captain, click "Captain Login" below to log in. If you're a supporter, click "Submit Report" to submit an anonymous report from your precinct.
+          If you're a precinct captain, click "Captain Login" below to log in. If you're a supporter, click "Submit Report" to submit a report from your precinct.
         </p>
         <hr />
         <p>
