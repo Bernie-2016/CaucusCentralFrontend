6a75d87c35d56c7b68bae1ce593986b7d6e2b2bc
Zach Schneider <zach@schneid.io>
2016-01-28 15:02:06 -0600

    Improve captain nav spacing

--- src/components/nav/CaptainNav.js
+++ src/components/nav/CaptainNav.js
@@ -5,21 +5,23 @@ import { Nav, Navbar, NavItem } from 'react-bootstrap';
 export default class CaptainNav extends React.Component {
   render() {
     return (
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to='/captain'>Caucus Central</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <NavItem eventKey={1} href='/captain/help'>Help</NavItem>
-            <NavItem eventKey={2} href='/captain/profile'>Profile</NavItem>
-            <NavItem eventKey={3} onClick={this.props.signOut} href='#'>Sign Out</NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className='row'>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to='/captain'>Caucus Central</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href='/captain/help'>Help</NavItem>
+              <NavItem eventKey={2} href='/captain/profile'>Profile</NavItem>
+              <NavItem eventKey={3} onClick={this.props.signOut} href='#'>Sign Out</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
