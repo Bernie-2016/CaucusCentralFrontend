59fafd07d633bbd2822de5d188beb8045b5651d6
Zach Schneider <zach@schneid.io>
2016-02-17 18:41:06 -0600

    Correctly redirect for admin

--- src/views/sign-in/SignInView.js
+++ src/views/sign-in/SignInView.js
@@ -31,7 +31,7 @@ class SignInView extends React.Component {
 
   redirectToDashboardIfLoggedIn () {
     if (this.props.id !== undefined) {
-      if (this.props.privilege === 'organizer') {
+      if (this.props.privilege === 'admin' || this.props.privilege === 'organizer') {
         this.props.history.pushState(null, '/admin');
       } else if (this.props.privilege === 'captain') {
         this.props.history.pushState(null, '/captain')
